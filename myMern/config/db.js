8ee9f1d8084090fad7ae0b8082ed74ab113983b9
Maneka123 <it19980782@my.sliit.lk>
2024-01-18 13:31:43 +0530

    commit one

---
+++ myMern/config/db.js
@@ -0,0 +1,19 @@
+// db.js
+
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://maneka:maneka123@mycluster.gpttvfg.mongodb.net/authDB?retryWrites=true&w=majority";
+/* Replace <password> with your database password */
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
